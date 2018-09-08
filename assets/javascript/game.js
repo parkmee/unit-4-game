571f6e5459de95e38ca7530c39318eb0ed39c817
MMPark <parkmee@gmail.com>
2018-09-08 12:48:09 -0400

    wip - addition of blue crystal values and counter number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var purple = 0;
     var red = 0;
     var targetNumber = "";
-    var counterNumber = "";
+    var counterNumber = 0;
     var newGame = true;
 
     var targetNumberText = $("#target-number-text");
@@ -15,10 +15,7 @@ $(document).ready(function () {
     var lossesText = $("#losses-text");
     var counterText = $("#counter-text");
     var crystal = $(".crystal");
-    var blueCrystal = $("#blue-crystal");
-    var clearCrystal = $("#clear-crystal");
-    var purpleCrystal = $("#purple-crystal");
-    var redCrystal = $("#red-crystal");
+
 
     function pickNumbers() {
         if (newGame) {
@@ -30,30 +27,24 @@ $(document).ready(function () {
             red = Math.floor(Math.random() * 9 + 1);
 
             // display targetNumber
-            targetNumberText.text("Target: "+targetNumber);
-            blueCrystal
+            targetNumberText.text("Target: " + targetNumber);
 
             console.log("target: " + targetNumber + " | blue: " + blue + " | clear: " + clear + " | purple: " + purple + " | red: " + red);
 
-            // assign values to crystals
-
             newGame = false;
-            counterText.empty();
+            counterText.html("<h2>Your total score is: </h2>")
         }
     }
 
-    function sumValue() {
-        console.log(this);
-    }
+    
+
+
 
     // on-click of crystal
     crystal.on("click", function () {
 
         // assign random numeric values to crystals and target number
         pickNumbers();
-
-        // add numeric value of crystal to counter
-        sumValue();
 
         // calculate and display counter value
 
@@ -64,4 +55,19 @@ $(document).ready(function () {
         // restart game
 
     })
+    var clearCrystal = $("#clear-crystal");
+    var purpleCrystal = $("#purple-crystal");
+    var redCrystal = $("#red-crystal");
+
+    $("#blue-crystal").on("click", function () {
+        counterNumber = counterNumber + blue;
+        console.log(counterNumber);
+        counterText.html("Your total score is: " + counterNumber);
+
+    });
+
+    $("#clear-crystal").on("click", function () {
+        
+    })
+
 })
